5ccda76fb12921ce354c671eb8131ff5aa06a907
Rohit Patil <rp7938008@gmail.com>
2025-09-16 14:06:35 +0530

    removed comments of js

--- app9.js
+++ app9.js
@@ -1,10 +1,11 @@
-// let smallImages = document.getElementsByClassName("oldImg");
-//     for(i=0;i<smallImages.length;i++){
-//         smallImages[i].src = "assets/spiderman_img.png";
-//         console.log(`value of the image ${i} is changed.`);
-//     }
-// let links = document.querySelectorAll(".box a");
+let smallImages = document.getElementsByClassName("oldImg");
+    for(i=0;i<smallImages.length;i++){
+        smallImages[i].src = "assets/spiderman_img.png";
+        console.log(`value of the image ${i} is changed.`);
+    }
+let links = document.querySelectorAll(".box a");
 
-// for(i=0;i<links.length;i++){
-// links[i].style.color = "yellow";
-// }
+for(i=0;i<links.length;i++){
+links[i].style.color = "yellow";
+}
+// removed comments
