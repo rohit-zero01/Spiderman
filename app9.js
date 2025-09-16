e49f6f759594af46c2be74a3a68e0555d4e50431
Rohit Patil <rp7938008@gmail.com>
2025-09-16 13:52:04 +0530

    first commit

---
+++ app9.js
@@ -0,0 +1,10 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+//     for(i=0;i<smallImages.length;i++){
+//         smallImages[i].src = "assets/spiderman_img.png";
+//         console.log(`value of the image ${i} is changed.`);
+//     }
+// let links = document.querySelectorAll(".box a");
+
+// for(i=0;i<links.length;i++){
+// links[i].style.color = "yellow";
+// }
